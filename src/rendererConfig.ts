aa7df6be1ea8879f17e506efece62e18fd71310f
YuriiDzyuba <iurii11022@gmail.com>
2022-05-23 19:01:17 +0300

    refactored rendererConfig

--- src/rendererConfig.ts
+++ src/rendererConfig.ts
@@ -2,24 +2,24 @@ import { RendererConfig } from './types/rendererConfig.type';
 
 export const rendererConfig: RendererConfig = {
   background: '#acc4e2',
-  font: 'bold 20pt Menlo',
   textColor: '#fff',
-  rectangleHeight: 45,
   lineColor: '#8165b8',
-  arrowColor: '#8165b8',
   participantBoxColor: '#26134b',
-  lineWidth: 4,
   columnWidth: 300,
-  relationHeight: 100,
-  topOffset: 50,
-  bottomOffset: 100,
+  relationHeight: 80,
   participant: {
     width: 300,
-    textColor: '#fff',
+    textColor: '#ffffff',
     rectangleHeight: 45,
     boxColor: '#26134b',
     lineColor: '#8165b8',
     lineWidth: 4,
     font: 'bold 20pt Menlo',
+  },
+  relation: {
+    arrowColor: '#950e0e',
+    textColor: '#950e0e',
+    lineWidth: 2,
+    font: 'bold 16pt Menlo',
   },
 };
