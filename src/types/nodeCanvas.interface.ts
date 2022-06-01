83008a69d7cccebbf2fbaacea4bf5fa3dfcd051e
yurii44 <yurii.dziuba@leverx.com>
2022-06-01 10:35:24 +0300

    Document rendererConfig.type.ts and nodeCanvas.interface.ts

--- src/types/nodeCanvas.interface.ts
+++ src/types/nodeCanvas.interface.ts
@@ -1,3 +1,10 @@
+/** NodeCanvas extends native HTMLCanvasElement with additional method toBuffer */
 export interface NodeCanvas extends HTMLCanvasElement {
-  toBuffer(mimeType?: string, config?: any): Buffer;
+  /** Creates a Buffer object representing the image contained in the canvas
+   * mimeType A string indicating the image format. Valid options are image/png, image/jpeg
+   * */
+  toBuffer(mimeType?: string): Buffer;
 }
+
+
+
--- src/types/rendererConfig.type.ts
+++ src/types/rendererConfig.type.ts
@@ -1,27 +1,53 @@
 export type ParticipantConfig = {
+  /** canvas with px */
   width: number;
+
+  /** tex color Hex*/
   textColor: string;
-  rectangleHeight: number;
+
+  /** participant text box height px*/
+  boxHeight: number;
+
+  /** participant text box color Hex*/
   boxColor: string;
+
+  /** line color Hex*/
   lineColor: string;
+
+  /** line width px*/
   lineWidth: number;
+
+  /** above line text font weight,size and type (bold 20pt Menlo)*/
   font: string;
 };
 
 export type RelationConfig = {
+  /** line and arrow color Hex*/
   arrowColor: string;
+
+  /** above line text color Hex*/
   textColor: string;
+
+  /** arrow line width px*/
   lineWidth: number;
+
+  /** above line text font weight,size and type (bold 20pt Menlo)*/
   font: string;
 };
 
-export type RendererConfig = {
+export type rendererConfig = {
+  /** canvas background*/
   background: string;
-  lineColor: string;
-  textColor: string;
-  participantBoxColor: string;
-  columnWidth: number;
+
+  /** one participant column width px*/
+  participantWidth: number;
+
+  /** one relation row height px*/
   relationHeight: number;
+
+  /** configs for participant instance*/
   participant: ParticipantConfig;
+
+  /** configs for relation instance*/
   relation: RelationConfig;
 };
