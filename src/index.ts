0fbe67ad5dc0e077ba21eb8c10d6b73ab80e6487
yurii44 <yurii.dziuba@leverx.com>
2022-06-06 11:15:50 +0300

    Fix reading file content outside a mrdParser

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { mrdParser } from './mrdParser';
 import { defaultRendererConfig } from './sequintence/DefaultRendererConfig';
 import * as path from 'path';
 
-const diagram = mrdParser(path.join(__dirname, 'source.mrd'));
+const sourseText = path.join(__dirname, 'source.mrd');
+const diagram = mrdParser(sourseText);
 
 const render = new Renderer(diagram, defaultRendererConfig);
 render.printDiagram();
